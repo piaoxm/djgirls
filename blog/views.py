f405049d36011711c90d48442cff2b03419d8491
piaoxm <min-e@nate.com>
2019-10-22 23:47:04 +0900

    blog advance

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def post_list(request):
@@ -11,20 +12,22 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-    
+
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)# POST로 받은 폼을 넘겨줌
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()  #이렇게 하면, 새로 작성한 글이 바로 게시되지 않음.
             post.save()
             return redirect('blog:post_detail', pk=post.pk)
     else:
         form = PostForm()# 빈 폼을 넘겨줌
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -32,7 +35,7 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()  #이렇게 하면, 새로 작성한 글이 바로 게시되지 않음.
             post.save()
             return redirect('blog:post_detail', pk=post.pk)
     else:
@@ -41,3 +44,20 @@ def post_edit(request, pk):
 
 def archives_year(request):
     return render(request, 'blog/post_list.html', {})
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish() # model 의 publish 함수 실행
+    return redirect('blog:post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('blog:post_list')
